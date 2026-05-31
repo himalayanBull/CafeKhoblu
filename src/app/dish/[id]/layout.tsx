c7c75bfa27a70a44eba13f0680612bbfbbb67b5d
Himalayan Bull <140274331+himalayanBull@users.noreply.github.com>
2026-05-31 09:15:36 +0100

    Delete src directory

--- src/app/dish/[id]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { dishes } from '@/data/dishes';
-
-export function generateStaticParams() {
-  return dishes.map((dish) => ({ id: dish.id }));
-}
-
-export default function DishLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
--- src/app/m/[code]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { menuCodes } from '@/data/menu-codes';
-
-export function generateStaticParams() {
-  return menuCodes.map((mc) => ({ code: mc.code }));
-}
-
-export default function MenuCodeLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
